2c14e03614e1d460d901f3d343efc33d6ce1cf9b
laplance <laplance@naver.com>
2020-04-02 09:45:00 +0900

    크로스브라우징 가능하도록 수정

--- web/handlers.go
+++ web/handlers.go
@@ -103,10 +103,12 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	if h.IsStatic {
 		// Instruct the browser not to display us in an iframe unless is the same origin for anti-clickjacking
-		w.Header().Set("X-Frame-Options", "SAMEORIGIN")
+		//w.Header().Set("X-Frame-Options", "SAMEORIGIN")
+		w.Header().Set("X-Frame-Options", "allow-from http://localhost:8080 http://10.3.1.15:8080 http://wooj.yht.co.kr")		
 		// Set content security policy. This is also specified in the root.html of the webapp in a meta tag.
 		w.Header().Set("Content-Security-Policy", fmt.Sprintf(
-			"frame-ancestors 'self'; script-src 'self' cdn.segment.com/analytics.js/%s",
+			//"frame-ancestors 'self'; script-src 'self' cdn.segment.com/analytics.js/%s",
+			"frame-ancestors 'self' http://localhost:8080 http://10.3.1.15:8080 http://wooj.yht.co.kr; script-src 'self' cdn.segment.com/analytics.js/%s",			
 			h.cspShaDirective,
 		))
 	} else {
